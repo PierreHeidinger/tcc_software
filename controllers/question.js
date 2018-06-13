f5fb96d15aec61e19ebf47b041a71fd7a1d1d1a0
Luis Zambrano Sandoval <lzambran@everis.com>
2018-06-13 10:04:37 -0300

    metodo LoadMassive - ahora generera los cuestionarios registrando su origen y estado

--- controllers/question.js
+++ controllers/question.js
@@ -1,81 +1,83 @@
 
 const Excel = require('../services/excel')
 const Files = require('../services/files');
-const { Question ,Profile , Group , Questionnaire } = require('../models/index');
+const { Question, Profile, Group, Questionnaire } = require('../models/index');
 
 
 /**
  * Public class Controller
  */
 
- class QuestionController {
-     
-    async List(req,res){
+class QuestionController {
+
+    async List(req, res) {
         const questions = await Question.find({}).exec();
         return res.status(200).json(questions);
     };
-    
-    async Create(req,res){
+
+    async Create(req, res) {
         const content = req.body;
         const question = new Question(content);
-    
+
         const newQuestion = await question.save();
-    
+
         return res.status(200).json(newQuestion);
     };
-    
-    async Update(req,res){
-    
+
+    async Update(req, res) {
+
         const id = req.params._id;
         const content = req.body;
-    
-        const question = await Question.findOneAndUpdate(id,content).exec();
-    
+
+        const question = await Question.findOneAndUpdate(id, content).exec();
+
         return res.status(200).json(question);
     };
-    
-    async Remove(req,res){
-    
+
+    async Remove(req, res) {
+
         const id = req.params._id;
-    
+
         const result = await Question.findByIdAndRemove(id);
-    
+
         return res.status(200).json(result);
     }
-    
-    async LoadMassive(req,res){
-        
+
+    async LoadMassive(req, res) {
+
         const dir = await Files.Save(req.files.questions);
-   
+
         const information = await Excel.Read(dir);
-    
+
         const lista = await readInformation(information);
-    
+
         var questionnaires = [];
-       
+        let az = await Questionnaire.update({ state: true, origin: "EXCEL" }, { $set: { state: false } }, { multi: true });
+
         for (let profile of lista.profiles) {
 
-            let  questionnaire = {             
-                questionnaireType : profile.description
+            let questionnaire = {
+                questionnaireType: profile.description
             };
 
 
             var questions = [];
-            
+
             console.log(lista.questions);
 
-            for(let question of lista.questions.filter(_question => _question.profile == profile.description )){
+            for (let question of lista.questions.filter(_question => _question.profile == profile.description)) {
 
                 console.log('asdas');
 
-                let localQuestion = { 
-                    text : question.content
-                 } 
-                 questions.push(localQuestion);
+                let localQuestion = {
+                    text: question.content
+                }
+                questions.push(localQuestion);
             }
 
-            questionnaire.questions = questions
-
+            questionnaire.questions = questions;
+            questionnaire.state = true;
+            questionnaire.origin = "EXCEL"
             questionnaires.push(questionnaire);
 
         }
@@ -83,87 +85,87 @@ const { Question ,Profile , Group , Questionnaire } = require('../models/index')
         await Questionnaire.insertMany(questionnaires);
 
         res.status(200).json("sending information of questions , waiting please");
-                
+
     }
-    
- }
+
+}
 
 /**
  * Private functions 
  */
-async function readInformation(docInformation){
-    
+async function readInformation(docInformation) {
+
     var questions = [];
     var profiles = [];
     var groups = [];
 
-    docInformation.forEach((sheet)=>{
+    docInformation.forEach((sheet) => {
 
-        sheet.content.forEach((model)=>{
-            let profile ={
-                "description" : model.Perfil
+        sheet.content.forEach((model) => {
+            let profile = {
+                "description": model.Perfil
             };
-    
+
             let group = {
-                "description" : model.Grupo,
-                "profile" : model.Perfil
+                "description": model.Grupo,
+                "profile": model.Perfil
             }
-    
+
             let question = {
-                "content" : model.Questão,
-                "group" : model.Grupo,
-                "profile" : model.Perfil
+                "content": model.Questão,
+                "group": model.Grupo,
+                "profile": model.Perfil
             }
-            
+
             var exists = profiles.find(x => x.description == model.Perfil);
-            if(!exists)profiles.push(profile);            
+            if (!exists) profiles.push(profile);
             groups.push(group);
             questions.push(question);
         })
 
     })
 
-    return {"questions" : questions , "profiles" : profiles , "groups" : groups };
+    return { "questions": questions, "profiles": profiles, "groups": groups };
 }
-async function createOrUpdateProfiles(profiles){
+async function createOrUpdateProfiles(profiles) {
 
-    for(var profile of profiles){
+    for (var profile of profiles) {
 
-        const filter = { description : profile.description.trim() };
-        const options = { upsert : true }
+        const filter = { description: profile.description.trim() };
+        const options = { upsert: true }
 
-        const createUpdate =  await Profile.update(filter,profile,options);
-    }   
+        const createUpdate = await Profile.update(filter, profile, options);
+    }
 
 }
-async function createOrUpdateGroups(groups){
+async function createOrUpdateGroups(groups) {
 
-    for(var group of groups){
+    for (var group of groups) {
 
-        group.profile = await Profile.findOne({ description : group.profile }) ;
-        
-        const createUpdate = await Group.update({ description : group.description.trim() , "profile.description": group.profile.description.trim() },group ,{ upsert : true });
+        group.profile = await Profile.findOne({ description: group.profile });
+
+        const createUpdate = await Group.update({ description: group.description.trim(), "profile.description": group.profile.description.trim() }, group, { upsert: true });
 
     }
 
 }
-async function createOrUpdateQuestions(questions){
+async function createOrUpdateQuestions(questions) {
 
-    for(var question of questions){
+    for (var question of questions) {
 
         const group = question.group;
         const profile = question.profile;
 
-        question.group = await Group.findOne({ "description" : group, "profile.description" : profile });
-        question.profile = await Profile.findOne({ "description" : profile })
+        question.group = await Group.findOne({ "description": group, "profile.description": profile });
+        question.profile = await Profile.findOne({ "description": profile })
 
-        const createUpdate = await Question.update({ "content" : question.content } , question , { upsert : true });
+        const createUpdate = await Question.update({ "content": question.content }, question, { upsert: true });
     }
 }
-async function load(lista){
+async function load(lista) {
 
     const profiles = await createOrUpdateProfiles(lista.profiles);
-    
+
     const groups = await createOrUpdateGroups(lista.groups);
 
     const question = await createOrUpdateQuestions(lista.questions);
