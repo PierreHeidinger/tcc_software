e5ddf59ae7b041801667e120ee3d98515a560b74
Alexis ZS <alexis.zs.1894@gmail>
2018-06-17 10:56:50 -0300

    Se agrega  atributos startDate y  endDate

--- models/questionnaires.js
+++ models/questionnaires.js
@@ -3,29 +3,35 @@ const Schema = mongoose.Schema;
 
 const questionnaireSchema = new Schema({
 
-    creationDate: { 
-        type: Date,
-        default: Date.now
-    },
-    updateDate: { 
-        type: Date,
-        default: Date.now
-    },
     questionnaireType: String,
     state: Boolean,
     origin: String,
     questions: [{
-        text:String,
-        questionType:{
-            type:String,
+        text: String,
+        questionType: {
+            type: String,
             default: "radio"
         },
         options: {
-            type:Array,
-            default : ["very unsatisfied","unsatisfied","indiffent/neutral","satisfied","very satisfied"]
+            type: Array,
+            default: ["very unsatisfied", "unsatisfied", "indiffent/neutral", "satisfied", "very satisfied"]
         }
-    }]
-    
+    }],
+    creationDate: {
+        type: Date,
+        default: Date.now
+    },
+    updateDate: {
+        type: Date,
+        default: Date.now
+    },
+    startDate: {
+        type: Date
+    },
+    endDate: {
+        type: Date
+    }
+
 });
 
-module.exports = mongoose.model('questionnaires',questionnaireSchema);
+module.exports = mongoose.model('questionnaires', questionnaireSchema);
